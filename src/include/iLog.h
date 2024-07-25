35468c6cb576edd800d07b514c927876f3c3219d
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-07-25 23:22:53 +0300

    "Add files for the iLog project"

---
+++ src/include/iLog.h
@@ -0,0 +1,6 @@
+#ifndef _ILOG_H_
+#define _ILOG_H_
+
+// code here
+
+#endif  // _ILOG_H_
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include "iLog.h"
+
+#include <stdio.h>
+
+int main(void) {
+  const char* msg = "Welcome in our log program.";
+  printf("%s",msg);
+  return 0;
+}
