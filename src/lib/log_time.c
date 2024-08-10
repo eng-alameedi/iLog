65346e06e1a6b6b541f8b7d4d185310899900b5f
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-08-10 16:47:52 +0300

    "Add local time file implementation"

---
+++ src/lib/log_time.c
@@ -0,0 +1,19 @@
+//
+// File: log_time.c
+//
+// This file is the implementation of the file log_time.h, which
+// contain the function required to get the current time, and date
+//
+
+#include "log_time.h"
+#include <time.h>
+
+char* current_time()
+{
+  time_t time_now = time(NULL);
+  struct tm *local_time = localtime(&time_now);
+
+  char* text_time;
+  strftime(text_time, sizeof(text_time), "%Y-%m-%d %H:%M:%S", local_time);
+  return text_time;
+}
